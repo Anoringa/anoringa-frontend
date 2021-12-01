2d248b33d0f068b79787e463f0f390ef17b5cc6d
olmedoluis <olmedoluis@gmail.com>
2021-12-01 19:32:18 -0300

    Properly set right colors

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -133,7 +133,7 @@
           <template #button-content>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 35.8 36"
+              viewBox="0 0 35.9 36"
               width="30"
               class="user-icon"
               style="display: inline-block"
