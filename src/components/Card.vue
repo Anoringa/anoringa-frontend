5d2bbee5765e161f8aa1947aae947c78d18a8a72
olmedoluis <olmedoluis@gmail.com>
2021-12-24 17:11:53 -0300

    Isolate card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,168 +1,115 @@
 <template>
-  <div class="card card-1 clickAble" :id="id" :href="postlink">
-    <!--
-    v-bind:style="'background-image: url(' + getPhoto() + ')'"
-    -->
-
-    <a
-      v-bind:href="'/post/' + id"
-      style="display: block; height: 100%; text-decoration: none; color: white"
+  <a
+    :id="id"
+    :href="urlEndpoint"
+    class="
+      col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1 col-12
+      project-card-wrapper
+    "
+  >
+    <div
+      class="col-12 project-card"
+      v-bind:style="{
+        background:
+          'rgb(255, 255, 255) url(' +
+          getPhoto(photo) +
+          ') no-repeat scroll center / cover',
+      }"
     >
-      <h3 style="" class="thumb-title">{{ title }}</h3>
-      <img
-        data-filter="overlay-dark"
-        class="post-image"
-        :alt="photodefault"
-        :src="getPhoto()"
-      />
-      <div class="thumb-anim">
-        <!--
-        <h3 class="thumb-title">{{ title }}</h3>
-        <p class="thumb-description">{{ content }}</p>
-        -->
+      <div class="project-card-content">
+        <h4 class="cardtitle">{{ title }}</h4>
+
+        <div class="h-100 d-inline-block pb-3 card-metadata">
+          <div v-if="postSort == 'lastComment'">
+            <p v-if="lastComment == ['2020-11-30T00:00:00.000Z']"></p>
+
+            <p v-else>
+              <b-icon icon="plus-circle" aria-hidden="true"></b-icon>
+              Comentado {{ lastCommentDate }}
+            </p>
+          </div>
+
+          <div v-else-if="postSort == 'countOfComments'">
+            <b-icon icon="chat-left-text" aria-hidden="true"></b-icon>
+            {{ countOfComments }} comentarios
+          </div>
+
+          <div v-else-if="postSort == 'updatedAt'">
+            <p>
+              <b-icon icon="chat-left-text" aria-hidden="true"></b-icon>
+              {{ countOfComments }}
+
+              <b-icon icon="pen" aria-hidden="true"></b-icon>
+              por: {{ authorUsername }}
+            </p>
+          </div>
+
+          <div v-else>
+            <b-icon icon="clock" aria-hidden="true"></b-icon>
+            {{ lastComment | moment }}
+          </div>
+        </div>
       </div>
-      <div class="thumb-author">{{ photo }}</div>
-      <div class="thumb-author">{{ getImageBrightness(getPhoto()) }}</div>
-    </a>
-  </div>
-
-  <!--
-
-:style="{ 'background-image':  photo }"
-  <div class="card clickAble" :id="id" @click="clicked()">
-    <div class="image">
-      <img v-bind:src="photo" alt="image" width="200" height="200" />
     </div>
-    <div class="content">title: {{ title }}</div>
-    <p class="content">content: {{ content }}</p>
-  </div>
-  -->
-
-  <!--
-  <div class="card clickAble" :id="id">
-    <div>
-      <div class="logo">
-        <img v-bind:src="photo" alt="logo" width="200" height="200" />
-      </div>
-      <h3 class="title">{{ title }}</h3>
-      <div class="subtitle">{{ content }}</div>
-    </div>
-  </div>
-      -->
-
-  <!--
-    <div class="image" @click="clicked()" :class="cssClasses" v-text="label">
-      <img v-bind:src="label" alt="image" width="800" height="600" />
-    </div>
-
-    <div class="content">{{ label }}</div>
-      -->
-
-  <!--
-    <footer class="container">
-      <div class="account">
-        <a class="register" href="#">Register</a>
-        <a class="login" href="#">Login</a>
-      </div>
-      <div class="icons">
-        <a href="#"><i class="fas fa-heart"></i></a>
-        <a href="#"><i class="fas fa-share-alt"></i></a>
-  </div>
-  </footer>
-      -->
+  </a>
 </template>
 
 
 <script>
-//https://es.vuejs.org/v2/guide/list.html
-//v-bind:style="{ 'background-image': 'url(' + photo + ')' }"
+import moment from "moment";
+
+moment.locale("es");
+
 export default {
+  name: "Card",
   props: {
     id: {
-      //type: String,
-      required: true,
-    },
-    url: {
-      type: String,
-      required: false,
-    } /*
-    message: {
       type: String,
       required: true,
     },
-    label: {
+    urlEndpoint: {
       type: String,
-      required: true,
+      default: "",
     },
-    cssClasses: {
-      type: String,
-      required: false,
-    },*/,
     photo: {
       type: [String, Object],
-      required: false,
     },
     title: {
       type: String,
-      required: false,
+      default: "",
     },
-    content: {
+    postSort: {
       type: String,
-      required: false,
+      default: "",
+    },
+    lastComment: {
+      type: String,
+      default: "",
+    },
+    countOfComments: {
+      type: String,
+      default: "",
+    },
+    authorUsername: {
+      type: String,
+      default: "",
     },
   },
   data() {
     return {
       photodefault: "https://picsum.photos/200?random=1",
       postlink: "/post/" + this.id,
+      lastCommentDate: "",
     };
   },
+  created() {
+    this.moment = moment;
+  },
   mounted() {
-    /*
-            window.EventHandler.listen('remove-dialog-' + this.id + '-called', (data) => {
-                window.location.reload(true);
-                console.log(data);
-            });*/
+    this.lastCommentDate = moment(this.lastComment).fromNow();
   },
   methods: {
-    getImageBrightness(imageSrc) {
-      return imageSrc;
-    },
-    getImageBrightness2(imageSrc) {
-      var img = document.createElement("img");
-      img.src = imageSrc;
-      img.style.display = "none";
-      document.body.appendChild(img);
-
-      var colorSum = 0;
-
-      img.onload = function () {
-        // create canvas
-        var canvas = document.createElement("canvas");
-        canvas.width = this.width;
-        canvas.height = this.height;
-
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(this, 0, 0);
-
-        var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        var data = imageData.data;
-        var r, g, b, avg;
-
-        for (var x = 0, len = data.length; x < len; x += 4) {
-          r = data[x];
-          g = data[x + 1];
-          b = data[x + 2];
-
-          avg = Math.floor((r + g + b) / 3);
-          colorSum += avg;
-        }
-
-        var brightness = Math.floor(colorSum / (this.width * this.height));
-        return brightness;
-      };
-    },
-    getPhoto() {
+    getPhoto(photosonic) {
       function validURL(str) {
         var pattern = new RegExp(
           "^(https?:\\/\\/)?" + // protocol
@@ -173,66 +120,31 @@ export default {
             "(\\#[-a-z\\d_]*)?$",
           "i"
         ); // fragment locator
+
         return !!pattern.test(str);
       }
 
       // Photo object vs media
-      if (typeof this.photo === "object") {
+      if (typeof photosonic === "object" || photosonic instanceof Object) {
         //console.log("the image coms from object");
-        if (validURL(this.photo.content)) {
-          return this.photo.content;
+        if (validURL(photosonic.content)) {
+          return photosonic.content;
         } else {
-          return this.photodefault;
+          return this.photosonicdefault;
         }
       } else if (
-        typeof this.photo === "string" ||
-        this.photo instanceof String
+        typeof photosonic === "string" ||
+        photosonic instanceof String
       ) {
         //console.log("the image coms from string");
-        if (validURL(this.photo)) {
-          return this.photo;
+        if (validURL(photosonic)) {
+          return photosonic;
         } else {
-          return this.photodefault;
+          return this.photosonicdefault;
         }
-      }else {
+      } else {
         console.log("anyone know the image coms from ");
-
-      } /*else {
-        console.log("anyone know the image coms from ");
-        if (validURL(this.photo.content)) {
-          return this.photo.content;
-        } else {
-          return this.photodefault;
-        }
       }
-      if (this.photo.content) {
-        if (validURL(this.photo.content)) {
-          return this.photo.content;
-        } else {
-          return this.photodefault;
-        }
-      }
-      else{
-        if (validURL(this.photo.content)) {
-          return this.photo;
-        } else {
-          return this.photodefault;
-        }
-
-      }*/
-    },
-    clicked() {
-      console.log("clicked");
-      console.log(this.id);
-      window.location.href = "/post/" + this.id;
-
-      /*
-                window.EventHandler.fire('top-confirm', {
-                    id: 'remove-dialog-' + this.id,
-                    message: this.message,
-                    url: this.url
-                });
-                */
     },
   },
 };
@@ -240,250 +152,39 @@ export default {
 
 
 <style lang="scss" scoped>
-$darker: #222;
-$dark: #555;
-$bright: #ddd;
-
-.thumb-title {
-  text-transform: uppercase;
-  padding: 10px;
-  word-wrap: break-word; /*
-  -webkit-text-fill-color: white;
-  -webkit-text-stroke-width: 2px;
-  -webkit-text-stroke-color: black;
-  text-shadow: 2px 2px #000000;*/
-  text-shadow: 3px 3px 3px #000000;
-
-  font-size: 24px;
-  position: absolute;
-  z-index: 3;
-  float: bottom;
-}
-
-.thumb-description {
-  padding: 10px 20px;
-  opacity: 0;
-  flex: 1;
-}
-
-.thumb-author {
-  padding: 100px;
-  position: relative;
-}
-
-.post-image {
-  /*
-    position: absolute;
-    left: -1000%;
-    right: -1000%;
-    top: -1000%;
-    bottom: -1000%;
-    margin: auto;
-    min-height: 100%;
-    min-width: 100%;
-    */
-  /*
-  margin-left: 50rm;
-  margin-right: 50rm;
-  height: 100%;
-  width: auto;
-  padding: auto;
-  */
-  z-index: -1;
-  background-color: rgba(0, 0, 0, 0.5);
-  filter: brightness(0.9);
-  /*
-  height: 100%;
-  width: auto;
-  padding: auto;
-  */
-  padding: 0px;
-  height: 100%;
-  width: auto;
-  margin-left: -50%;
-  margin-right: -50%;
-}
-.card {
-  background: #fff;
-  border-radius: 2px;
-  /*
-  background-image: url("http://placekitten.com/300/300");
-  display: inline-block;
-  */
-  display: inline-block;
-  height: 200px;
-  width: 200px;
-  margin: 0.5rem;
-  position: relative;
-
-  /*
-  */
-  flex-flow: column nowrap;
-  overflow: hidden;
-  color: #fff;
-}
-
-.card-1 {
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
-}
-
-.card-1:hover {
-  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-}
-
-.card-2 {
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-}
-
-.card-3 {
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-}
-
-.card-4 {
-  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-}
-
-.card-5 {
-  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
-}
-
-.thumb-gradient,
-.thumb-img {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 300px;
-  width: 300px;
-  object-fit: cover;
-}
-
-/*
-.thumb-anim {
-  margin-top: auto;  
-  position: relative;
-  transition: 0.5s;
-  transform: translateY(100%);
-}
-
-.thumb-title {
-  text-transform: uppercase;
-  padding: 10px;
-  transition: 0.5s;
-  transform: translateY(-100%);
-}
-
-.thumb-description {
-  padding: 10px 20px;
-  transition: 0.5s;
-  opacity: 0;
-  flex: 1;
-}
-
-.thumb-author {
-  padding: 100px;
-  position: relative;
-}
-
-.card:hover .thumb-anim {
-  transform: translateY(0);
-}
-
-.card:hover .thumb-title {
-  transform: translateY(0%);
-}
-
-.card:hover .thumb-description {
+.project-card-content {
+  color: white;
   opacity: 1;
-}*/
-/*
-img {
-  max-width: 100%;
-  height: auto;
-  display: block;
+  position: absolute;
+  bottom: 0px;
+  text-overflow: ellipsis;
 }
-a {
-  text-decoration: none;
-  &:hover {
+
+.cardtitle {
+  font-weight: bold;
+  @include dynamic-theme() {
+    color: theme($card-foreground-color);
+  }
+}
+.card-metadata p {
+  @include dynamic-theme() {
+    color: theme($card-foreground-color);
   }
 }
 
-.card {
-  display: grid;
-  grid-template-rows: auto auto 1fr auto;
-  background: white;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-  min-width: 200px;
-  &:hover {
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.25);
-  }
-  header {
-    display: grid;
-    grid-template-columns: 48px 1fr;
-    grid-template-rows: 24px 24px;
-    grid-template-areas: "logo title" "logo subtitle";
-    padding: 0.5rem;
-    align-items: center;
-    .logo {
-      grid-area: logo;
-      img {
-        border-radius: 50%;
-        padding: 0.25rem;
-      }
-    }
-    .title,
-    .subtitle {
-      padding: 0.25rem;
-    }
-    .title {
-      grid-area: title;
-      color: $darker;
-    }
-    .subtitle {
-      grid-area: subtitle;
-    }
-  } // End Card Header
-  .image {
-    overflow: hidden;
-    img {
-      filter: grayscale(50%);
-      transition: all 0.5s ease;
-      cursor: pointer;
-      &:hover {
-        filter: none;
-      }
-    }
-  }
+@media only screen and (min-width: 600px) {
+  .project-card-wrapper {
+    margin: 0.3% 0%;
 
-  // End card image
-  .content,
-  footer {
-    padding: 1em;
+    padding-left: 8px !important;
+    padding-right: 8px !important;
   }
-  footer {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    a {
-      text-transform: uppercase;
-      padding-right: 0.5rem;
-      color: $dark;
-      font-weight: 500;
-      &:hover {
-        color: $darker;
-      }
-    }
-    i {
-      font-size: 1.2rem;
-      padding-left: 0.5rem;
-      color: $dark;
-      transition: 0.3s;
-      &:hover {
-        color: $darker;
-      }
-    }
-  } // End Card Footer
-} // End Card
-*/
+}
+
+@media only screen and (max-width: 1440px) and (min-width: 1200px) {
+  .cardtitle {
+    font-weight: initial;
+    font-size: 1.3rem;
+  }
+}
 </style>
